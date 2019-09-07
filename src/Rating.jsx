a1211448d2985f8b52ddfc31b37efb6db1b73952
Prusch, Maximilian <maximilian.prusch@outlook.com>
2019-09-07 23:21:32 +0200

    Add bandName and comment

--- src/Rating.jsx
+++ src/Rating.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
 import { Rating as RatingMaterialUI } from '@material-ui/lab';
+import { Grid, TextField } from '@material-ui/core';
+
 
 const Rating = () => {
+  const [bandName, setBandName] = useState('');
   const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
   return (
-    <RatingMaterialUI value={rating} onChange={(event, value) => setRating(value)} />
+    <Grid container justify="center" alignItems="center">
+      <Grid item xs={2}>
+        <TextField
+          id="bandName"
+          variant="outlined"
+          label="Band Name"
+          value={bandName}
+          onChange={event => setBandName(event.target.value)}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <RatingMaterialUI value={rating} onChange={(event, value) => setRating(value)} />
+      </Grid>
+      <Grid item xs={5}>
+        <TextField
+          id="comment"
+          fullWidth
+          variant="outlined"
+          label="Comment"
+          value={comment}
+          onChange={event => setComment(event.target.value)}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/Rating.test.jsx
+++ src/Rating.test.jsx
@@ -4,14 +4,42 @@ import toJSON from 'enzyme-to-json';
 import { Rating as RatingMaterialUI } from '@material-ui/lab';
 import Rating from './Rating';
 
-
+let wrapper;
 describe('Rating', () => {
-  it('should render correctly', () => {
-    expect(toJSON(shallow(<Rating />))).toMatchSnapshot();
+  beforeEach(() => {
+    wrapper = shallow(<Rating />);
   });
-  it('should update rating onChange', () => {
-    const wrapper = shallow(<Rating />);
-    wrapper.find(RatingMaterialUI).prop('onChange')({}, 2);
-    expect(wrapper.find(RatingMaterialUI).prop('value')).toBe(2);
+  it('should render correctly', () => {
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  describe('bandName', () => {
+    it('should have correct initial value', () => {
+      expect(wrapper.find('#bandName').prop('value')).toBe('');
+    });
+    it('should update TextField onChange', () => {
+      wrapper.find('#bandName').prop('onChange')({ target: { value: 'bandName' } });
+      expect(wrapper.find('#bandName').prop('value')).toBe('bandName');
+    });
+  });
+
+  describe('rating', () => {
+    it('should have correct initial state', () => {
+      expect(wrapper.find(RatingMaterialUI).prop('value')).toBe(0);
+    });
+    it('should update rating onChange', () => {
+      wrapper.find(RatingMaterialUI).prop('onChange')({}, 2);
+      expect(wrapper.find(RatingMaterialUI).prop('value')).toBe(2);
+    });
+  });
+
+  describe('comment', () => {
+    it('should have correct initial state', () => {
+      expect(wrapper.find('#comment').prop('value')).toBe('');
+    });
+    it('should update rating onChange', () => {
+      wrapper.find('#comment').prop('onChange')({ target: { value: 'comment' } });
+      expect(wrapper.find('#comment').prop('value')).toBe('comment');
+    });
   });
 });
