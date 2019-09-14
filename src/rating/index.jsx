e281e4dcd0844d2805baad01f670f9b0390d2d38
Prusch, Maximilian <maximilian.prusch@outlook.com>
2019-09-14 23:19:31 +0200

    Add form to submit rating

--- src/rating/index.jsx
+++ src/rating/index.jsx
@@ -1,39 +1,58 @@
 import React, { useState } from 'react';
 import { Rating as RatingMaterialUI } from '@material-ui/lab';
 import { Grid, TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { API, Auth } from 'aws-amplify';
 
 import './rating.css';
 
-
 const Rating = () => {
   const [bandName, setBandName] = useState('');
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(1);
   const [comment, setComment] = useState('');
+
+  const submitRating = async (event) => {
+    event.preventDefault();
+    if (bandName && bandName.trim()) {
+      const currentSession = await Auth.currentSession();
+      const token = currentSession.getAccessToken().getJwtToken();
+      await API.post('musicrating', '/bands', {
+        header: { Authorization: `Bearer ${token}` },
+        body: { bandName, rating, comment },
+      });
+    }
+  };
+
   return (
-    <Grid className="rating-container" container justify="center" alignItems="center">
-      <Grid item xs={2}>
-        <TextField
-          id="bandName"
-          variant="outlined"
-          label="Band Name"
-          value={bandName}
-          onChange={event => setBandName(event.target.value)}
-        />
+    <form id="rating-form" onSubmit={submitRating}>
+      <Grid className="rating-container" container justify="center" alignItems="center" spacing={5}>
+        <Grid item xs={2}>
+          <TextField
+            id="bandName"
+            variant="outlined"
+            label="Band Name"
+            value={bandName}
+            onChange={event => (setBandName(event.target.value))}
+          />
+        </Grid>
+        <Grid item className="rating-rating">
+          <RatingMaterialUI name="rating" value={rating} onChange={(event, value) => setRating(value)} />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            id="comment"
+            fullWidth
+            variant="outlined"
+            label="Comment"
+            value={comment}
+            onChange={event => setComment(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={1}>
+          <Button type="submit" variant="outlined"> Submit </Button>
+        </Grid>
       </Grid>
-      <Grid item className="rating-rating">
-        <RatingMaterialUI value={rating} onChange={(event, value) => setRating(value)} />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="comment"
-          fullWidth
-          variant="outlined"
-          label="Comment"
-          value={comment}
-          onChange={event => setComment(event.target.value)}
-        />
-      </Grid>
-    </Grid>
+    </form>
   );
 };
 
--- src/rating/index.test.jsx
+++ src/rating/index.test.jsx
@@ -2,19 +2,56 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import { Rating as RatingMaterialUI } from '@material-ui/lab';
+import { API, Auth } from 'aws-amplify';
 import Rating from './index';
 
 let wrapper;
 describe('Rating', () => {
   beforeEach(() => {
     wrapper = shallow(<Rating />);
+    jest.clearAllMocks();
   });
   it('should render correctly', () => {
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
+  describe('form', () => {
+    it('should sent data to api and should call event.preventDefault', async () => {
+      const preventDefaultMock = jest.fn();
+      const currentSessionMock = Promise.resolve({
+        getAccessToken: () => ({ getJwtToken: () => ('Token') }),
+      });
+      const expectedInit = {
+        header: { Authorization: 'Bearer Token' },
+        body: { bandName: 'bandName', rating: 4, comment: 'comment' },
+      };
+      const postSpy = jest.spyOn(API, 'post').mockImplementation(f => f);
+      jest.spyOn(Auth, 'currentSession').mockImplementation(() => currentSessionMock);
+      wrapper.find('#bandName').prop('onChange')({ target: { value: 'bandName' } });
+      wrapper.find(RatingMaterialUI).prop('onChange')({}, 4);
+      wrapper.find('#comment').prop('onChange')({ target: { value: 'comment' } });
+
+      await wrapper.find('#rating-form').prop('onSubmit')({ preventDefault: preventDefaultMock });
+      expect(preventDefaultMock).toHaveBeenCalled();
+      expect(postSpy).toHaveBeenCalledWith('musicrating', '/bands', expectedInit);
+    });
+
+    it('should not submit data if bandName is not filled', async () => {
+      const preventDefaultMock = jest.fn();
+      const postSpy = jest.spyOn(API, 'post').mockImplementation(f => f);
+      wrapper.find('#bandName').prop('onChange')({ target: { value: '' } });
+      await wrapper.find('#rating-form').prop('onSubmit')({ preventDefault: preventDefaultMock });
+      wrapper.find('#bandName').prop('onChange')({ target: { value: '  ' } });
+      await wrapper.find('#rating-form').prop('onSubmit')({ preventDefault: preventDefaultMock });
+      wrapper.find('#bandName').prop('onChange')({ target: { value: undefined } });
+      await wrapper.find('#rating-form').prop('onSubmit')({ preventDefault: preventDefaultMock });
+      expect(preventDefaultMock).toHaveBeenCalledTimes(3);
+      expect(postSpy).not.toHaveBeenCalled();
+    });
+  });
+
   describe('bandName', () => {
-    it('should have correct initial value', () => {
+    it('should have empty string as initial value', () => {
       expect(wrapper.find('#bandName').prop('value')).toBe('');
     });
     it('should update TextField onChange', () => {
@@ -24,8 +61,8 @@ describe('Rating', () => {
   });
 
   describe('rating', () => {
-    it('should have correct initial state', () => {
-      expect(wrapper.find(RatingMaterialUI).prop('value')).toBe(0);
+    it('should have 0 as initial state', () => {
+      expect(wrapper.find(RatingMaterialUI).prop('value')).toBe(1);
     });
     it('should update rating onChange', () => {
       wrapper.find(RatingMaterialUI).prop('onChange')({}, 2);
@@ -34,7 +71,7 @@ describe('Rating', () => {
   });
 
   describe('comment', () => {
-    it('should have correct initial state', () => {
+    it('should have empty string as initial state', () => {
       expect(wrapper.find('#comment').prop('value')).toBe('');
     });
     it('should update rating onChange', () => {
