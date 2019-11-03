09dde73c9f5a1ab988fe5334771556836e9fc60b
Maximilian Prusch <maximilian.prusch@outlook.com>
2019-11-03 23:42:24 +0100

    Refactor overview page and add table features

--- src/overview/index.jsx
+++ src/overview/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import {
-  Table, TableBody, TableCell, TableRow,
-} from '@material-ui/core';
+import React, { forwardRef } from 'react';
 import { API, Auth } from 'aws-amplify';
 import { Rating } from '@material-ui/lab';
+import MaterialTable from 'material-table';
 
+import {
+  ArrowUpward, ChevronLeft, ChevronRight, Clear, FirstPage, LastPage, Search,
+} from '@material-ui/icons';
+
+const tableIcons = {
+  /* eslint-disable react/jsx-props-no-spreading */
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+  /* eslint-enable react/jsx-props-no-spreading */
+};
 class Overview extends React.Component {
   constructor(props) {
     super(props);
@@ -25,20 +38,20 @@ class Overview extends React.Component {
     return (
       <>
         {ratings && (
-          <Table>
-            <TableBody>
-              {ratings.map((rating) => (
-                <TableRow key={rating.band}>
-                  <TableCell>{rating.band}</TableCell>
-                  <TableCell>{rating.festival}</TableCell>
-                  <TableCell>{rating.year}</TableCell>
-                  <TableCell>
-                    <Rating value={rating.rating} readOnly />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <MaterialTable
+            columns={[
+              { title: 'Band', field: 'band' },
+              { title: 'Festival', field: 'festival' },
+              { title: 'Year', field: 'year' },
+              {
+                title: 'Rating',
+                field: 'rating',
+                render: (data) => (<Rating name={data.band} value={data.rating} readOnly />),
+              }]}
+            data={ratings}
+            icons={tableIcons}
+            options={{ showTitle: false, draggable: false, pageSize: 10 }}
+          />
         )}
       </>
     );
--- src/overview/index.test.jsx
+++ src/overview/index.test.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import { API, Auth } from 'aws-amplify';
+import MaterialTable from 'material-table';
 import Overview from './index';
 
-const expectedRatings = [{ band: 'band', rating: 5 }];
+const expectedRatings = [{
+  band: 'band', festival: 'Wacken', year: 2014, rating: 5,
+}];
 
 describe('Overview', () => {
   let getBandsSpy;
@@ -22,6 +25,17 @@ describe('Overview', () => {
     const wrapper = shallow(<Overview />);
     process.nextTick(() => {
       expect(toJSON(wrapper)).toMatchSnapshot();
+      done();
+    });
+  });
+
+  it('should render rating correctly', (done) => {
+    const wrapper = shallow(<Overview />);
+    process.nextTick(() => {
+      const ratingComponent = shallow(wrapper.find(MaterialTable).prop('columns').find((column) => column.field === 'rating').render(expectedRatings[0]));
+      expect(ratingComponent.prop('name')).toEqual('band');
+      expect(ratingComponent.prop('value')).toEqual(5);
+      expect(ratingComponent.prop('readOnly')).toBeTruthy();
       done();
     });
   });
