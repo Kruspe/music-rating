316f07bb0572456d57b24b250e0a480734ba63fd
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-01-26 22:53:47 +0100

    Use Context in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,25 @@ import PropTypes from 'prop-types';
 import awsExports from './aws-exports';
 import './App.css';
 import TabBar from './TabBar';
+import UserProvider from './provider/UserProvider';
 
 Amplify.configure(awsExports);
 
 const App = (props) => {
   const { authState } = props;
-  return (authState === 'signedIn' && <TabBar />);
+  return (authState === 'signedIn' && (
+  <UserProvider>
+    <TabBar />
+  </UserProvider>
+  ));
 };
 
 App.propTypes = {
   authState: PropTypes.string,
+};
+
+App.defaultProps = {
+  authState: 'signedOut',
 };
 
 export default App;
