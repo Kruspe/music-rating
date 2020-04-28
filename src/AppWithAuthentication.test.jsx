32cd39fa736335238e8988243fb87914a39f990b
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-04-29 00:42:33 +0200

    Refactor tests and add assertions

--- src/AppWithAuthentication.test.jsx
+++ src/AppWithAuthentication.test.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { render, waitForElement, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import AppWithAuthentication from './AppWithAuthentication';
 
 describe('Authentication', () => {
   it('should display SignIn form', async () => {
-    const { findByText } = render(<AppWithAuthentication />);
-    expect(await waitForElement(() => findByText(/sign in$/i))).toBeVisible();
+    render(<AppWithAuthentication />);
+    expect(await screen.findByText(/sign in to your account/i)).toBeVisible();
+    expect(screen.getByPlaceholderText(/enter your username/i)).toBeVisible();
+    expect(screen.getByPlaceholderText(/enter your password/i)).toBeVisible();
+    expect(screen.getByText(/reset password/i)).toBeVisible();
+    expect(screen.getByText(/create account/i)).toBeVisible();
+    expect(screen.getByText('Sign In')).toBeVisible();
   });
 
   it('should display correct SignUp form', async () => {
-    const { findByText } = render(<AppWithAuthentication />);
-    const signUpLink = await waitForElement(() => findByText('Create account'));
-    fireEvent.click(signUpLink);
-    expect(await waitForElement(() => findByText(/create account/i))).toBeVisible();
+    render(<AppWithAuthentication />);
+    fireEvent.click(await screen.findByText(/create account/i));
+
+    expect(await screen.findByText(/create a new account/i)).toBeVisible();
+    expect(screen.getByPlaceholderText(/username/i)).toBeVisible();
+    expect(screen.getByPlaceholderText(/password/i)).toBeVisible();
+    expect(screen.getByPlaceholderText(/email/i)).toBeVisible();
+    expect(screen.queryByPlaceholderText(/phone number/i)).toBeNull();
+    expect(screen.getByText(/sign in/i)).toBeVisible();
+    expect(screen.getByText('Create Account')).toBeVisible();
   });
 });
