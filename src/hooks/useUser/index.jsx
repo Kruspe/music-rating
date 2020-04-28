a5952e03901fbe5e23699659198f12016210ac8e
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-04-29 00:13:51 +0200

    Add react-query and use it for fetching user data

---
+++ src/hooks/useUser/index.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+import { API, Auth } from 'aws-amplify';
+
+const getUserId = async () => {
+  const { id } = await Auth.currentUserInfo();
+  return id;
+};
+
+const getToken = async () => {
+  const currentSession = await Auth.currentSession();
+  return currentSession.getAccessToken().getJwtToken();
+};
+
+const getRatedArtists = async (key, userId, token) => API.get('musicrating',
+  `/api/v1/ratings/bands/${userId}`,
+  { header: { Authorization: `Bearer ${token}` } });
+
+const useUser = () => {
+  const { data: userId } = useQuery('userId', getUserId);
+  const { data: token } = useQuery('token', getToken);
+  const { data: ratedArtists } = useQuery(userId && token && ['ratedArtists', userId, token], getRatedArtists);
+
+  return { userId, token, ratedArtists };
+};
+
+export default useUser;
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,6 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+import { queryCache } from 'react-query';
+
+beforeEach(() => {
+  queryCache.clear();
+});
