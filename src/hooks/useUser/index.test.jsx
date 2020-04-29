190bdfc9701d792481d11285f80ae3de0f004063
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-04-29 21:46:58 +0200

    Add default mocks

--- src/hooks/useUser/index.test.jsx
+++ src/hooks/useUser/index.test.jsx
@@ -3,22 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { API, Auth } from 'aws-amplify';
 import useUser from './index';
 
-const mockGetJwtToken = jest.fn();
-jest.mock('aws-amplify', () => ({
-  Auth:
-    {
-      currentSession: async () => ({
-        getAccessToken: () => (
-          { getJwtToken: mockGetJwtToken }),
-      }),
-      currentUserInfo: jest.fn(),
-    },
-  API: {
-    get: jest.fn(),
-  },
-}));
-
-
 const UseUserHookExample = () => {
   const { userId, token, ratedArtists } = useUser();
   return (
@@ -31,44 +15,40 @@ const UseUserHookExample = () => {
 };
 
 describe('useUser', () => {
-  beforeEach(() => {
-    mockGetJwtToken.mockReset();
-    Auth.currentUserInfo.mockReset();
-    API.get.mockReset();
-  });
-
   it('should get userId, token and ratedArtists', async () => {
-    API.get.mockResolvedValueOnce([{
-      band: 'Bloodbath', festival: 'Wacken', year: 2015, rating: 5, comment: 'comment',
-    }]);
-    Auth.currentUserInfo.mockResolvedValueOnce({ id: 'userId' });
-    mockGetJwtToken.mockReturnValueOnce('token');
+    const getJwtTokenMock = jest.fn().mockReturnValueOnce('token');
+    Auth.currentSession.mockReturnValueOnce({
+      getAccessToken: () => ({ getJwtToken: getJwtTokenMock }),
+    });
     render(<UseUserHookExample />);
 
     expect(await screen.findByText('userId')).toBeInTheDocument();
     expect(await screen.findByText('token')).toBeInTheDocument();
     expect(await screen.findByText('Bloodbath')).toBeInTheDocument();
     expect(Auth.currentUserInfo).toHaveBeenCalledTimes(1);
-    expect(mockGetJwtToken).toHaveBeenCalledTimes(1);
+    expect(getJwtTokenMock).toHaveBeenCalledTimes(1);
     expect(API.get).toHaveBeenCalledWith('musicrating', '/api/v1/ratings/bands/userId', { header: { Authorization: 'Bearer token' } });
     expect(API.get).toHaveBeenCalledTimes(1);
   });
 
   it('should not get ratedArtists when userId was not fetched', async () => {
     Auth.currentUserInfo.mockResolvedValueOnce({ id: '' });
-    mockGetJwtToken.mockReturnValueOnce('token');
     render(<UseUserHookExample />);
 
     expect(await screen.findByText('token')).toBeInTheDocument();
+    expect(screen.queryByText('userId')).toBeNull();
     expect(API.get).not.toHaveBeenCalled();
   });
 
   it('should not get ratedArtists when token was not fetched', async () => {
-    Auth.currentUserInfo.mockResolvedValueOnce({ id: 'userId' });
-    mockGetJwtToken.mockReturnValueOnce('');
+    const getJwtTokenMock = jest.fn().mockReturnValueOnce('');
+    Auth.currentSession.mockReturnValueOnce({
+      getAccessToken: () => ({ getJwtToken: getJwtTokenMock }),
+    });
     render(<UseUserHookExample />);
 
     expect(await screen.findByText('userId')).toBeInTheDocument();
+    expect(screen.queryByText('token')).toBeNull();
     expect(API.get).not.toHaveBeenCalled();
   });
 });
