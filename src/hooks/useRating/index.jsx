3ae49f531f8e6c1a5f635fb438988b7d5c9e2abd
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-05-02 22:32:18 +0200

    Refactor and seperate hooks

---
+++ src/hooks/useRating/index.jsx
@@ -0,0 +1,14 @@
+import { useQuery } from 'react-query';
+import { API } from 'aws-amplify';
+import useUser from '../useUser';
+
+const getRatedArtists = async (key, userId, token) => (API.get('musicrating',
+  `/api/v1/ratings/bands/${userId}`,
+  { header: { Authorization: `Bearer ${token}` } }));
+
+const useRating = () => {
+  const { userId, token } = useUser();
+  return useQuery(userId.data && token.data && ['ratedArtists', userId.data, token.data], getRatedArtists);
+};
+
+export default useRating;
--- src/hooks/useUser/index.jsx
+++ src/hooks/useUser/index.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from 'react-query';
-import { API, Auth } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 
 const getUserId = async () => {
   const { id } = await Auth.currentUserInfo();
@@ -11,15 +11,10 @@ const getToken = async () => {
   return currentSession.getAccessToken().getJwtToken();
 };
 
-const getRatedArtists = async (key, userId, token) => API.get('musicrating',
-  `/api/v1/ratings/bands/${userId}`,
-  { header: { Authorization: `Bearer ${token}` } });
-
 const useUser = () => {
-  const { data: userId } = useQuery('userId', getUserId);
-  const { data: token } = useQuery('token', getToken);
-  const { data: ratedArtists = [] } = useQuery(userId && token && ['ratedArtists', userId, token], getRatedArtists);
-  return { userId, token, ratedArtists };
+  const userId = useQuery('userId', getUserId);
+  const token = useQuery('token', getToken);
+  return { userId, token };
 };
 
 export default useUser;
