92e10c57b8d9a68793395e7e7109007cfbc7ae94
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-05-02 23:23:02 +0200

    Refactor wacken estimation

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,18 +1,21 @@
 import '@testing-library/jest-dom/extend-expect';
 import { queryCache } from 'react-query';
-import { API, Auth } from 'aws-amplify';
-import { ApiGetMock, ApiPostMock } from './test/mocks';
+import { API, Auth, Storage } from 'aws-amplify';
+import { apiGetMock, apiPostMock, fetchMock, storageGetMock } from './test/mocks';
 
 beforeEach(() => {
   queryCache.clear();
 
-  jest.spyOn(API, 'get').mockImplementation(ApiGetMock);
-  jest.spyOn(API, 'post').mockImplementation(ApiPostMock);
+  window.fetch = jest.fn().mockImplementation(fetchMock);
+
+  jest.spyOn(API, 'get').mockImplementation(apiGetMock);
+  jest.spyOn(API, 'post').mockImplementation(apiPostMock);
   jest.spyOn(Auth, 'currentUserInfo').mockResolvedValue({ id: 'userId' });
   const currentSessionMock = {
     getAccessToken: () => ({ getJwtToken: () => ('token') }),
   };
   jest.spyOn(Auth, 'currentSession').mockResolvedValue(currentSessionMock);
+  jest.spyOn(Storage, 'get').mockImplementation(storageGetMock);
 });
 
 afterEach(() => {
--- src/tabs/wacken/index.test.jsx
+++ src/tabs/wacken/index.test.jsx
@@ -1,74 +1,30 @@
 import React from 'react';
 import {
-  act, fireEvent, render, wait,
+  fireEvent, render, screen, wait,
 } from '@testing-library/react';
-import { API, Storage } from 'aws-amplify';
-import EstimateWacken from './index';
-import UserContext from '../../context/UserContext';
+import Wacken from './index';
 
-describe('EstimateWacken', () => {
-  describe('Wacken', () => {
-    beforeEach(() => {
-      jest.spyOn(Storage, 'get').mockResolvedValueOnce('www.link-to-json.com');
-    });
-    it('should display unrated bands', async () => {
-      global.fetch = jest.fn().mockResolvedValueOnce(new Response(JSON.stringify(
-        [
-          { artist: 'Bloodbath', image: 'bloodbathImage' },
-          { artist: 'Megadeth', image: 'megadethImage' },
-          { artist: 'Vader', image: 'vaderImage' }],
-      )));
-      const {
-        getByText, queryByText, getByAltText, queryByAltText,
-      } = render(
-        <UserContext.Provider value={{ ratedArtists: [{ band: 'Bloodbath' }] }}>
-          <EstimateWacken />
-        </UserContext.Provider>,
-      );
+jest.mock('../../components/rating', () => (() => (<p>Rating</p>)));
 
-      await wait(() => {
-        expect(global.fetch).toHaveBeenCalledTimes(1);
-        expect(global.fetch).toHaveBeenCalledWith('www.link-to-json.com',
-          { headers: { 'Content-Type': 'application/json' } });
-      });
-      expect(queryByText(/bloodbath/i)).toBeNull();
-      expect(getByText('Megadeth')).toBeVisible();
-      expect(getByText('Vader')).toBeVisible();
-      expect(queryByAltText(/bloodbath/i)).toBeNull();
-      expect(getByAltText('Megadeth')).toBeVisible();
-      expect(getByAltText('Vader')).toBeVisible();
+describe('Wacken', () => {
+  it('should display unrated artists and rating', async () => {
+    render(<Wacken />);
+
+    await wait(() => {
+      expect(window.fetch).toHaveBeenCalledWith('wackenLink',
+        { headers: { 'Content-Type': 'application/json' } });
     });
-    it('should remove unrated band after rating', async () => {
-      global.fetch = jest.fn().mockResolvedValueOnce(new Response(JSON.stringify(
-        [
-          { artist: 'Vader', image: 'vaderImage' }],
-      )));
-      const postSpy = jest.spyOn(API, 'post').mockImplementation((f) => f);
-      const expectedInit = {
-        header: { Authorization: 'Bearer token' },
-        body: {
-          user: 'userId', band: 'Vader', festival: 'Wacken', year: '2015', rating: 5, comment: 'comment',
-        },
-      };
-      const {
-        findByText, getByLabelText, getByText, queryByText,
-      } = render(
-        <UserContext.Provider value={{ userId: 'userId', jwtToken: 'token', ratedArtists: [] }}>
-          <EstimateWacken />
-        </UserContext.Provider>,
-      );
-      expect(await findByText(/vader/i)).toBeVisible();
-      fireEvent.click(getByText(/vader/i));
-      fireEvent.change(getByLabelText(/festival \*/i), { target: { value: 'Wacken' } });
-      fireEvent.change(getByLabelText(/year \*/i), { target: { value: '2015' } });
-      fireEvent.click(getByLabelText(/5 star/i));
-      fireEvent.change(getByLabelText(/comment/i), { target: { value: 'comment' } });
-      await act(async () => {
-        fireEvent.submit(getByText(/submit/i));
-      });
-      await wait(() => expect(postSpy).toHaveBeenCalledTimes(1));
-      await wait(() => expect(postSpy).toHaveBeenCalledWith('musicrating', '/api/v1/ratings/bands', expectedInit));
-      expect(queryByText(/band/i)).not.toBeInTheDocument();
-    });
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    const megadethTile = await screen.findByText('Megadeth');
+    expect(megadethTile).toBeVisible();
+    expect(screen.getByText('Vader')).toBeVisible();
+    expect(screen.queryByText(/bloodbath/i)).toBeNull();
+    expect(screen.getByAltText('Megadeth')).toBeVisible();
+    expect(screen.getByAltText('Vader')).toBeVisible();
+    expect(screen.queryByAltText(/bloodbath/i)).toBeNull();
+
+    fireEvent.click(megadethTile);
+    expect(screen.getByText('Rating')).toBeInTheDocument();
   });
 });
