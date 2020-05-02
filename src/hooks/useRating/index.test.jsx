74f2cd4b1a85b3a774d4fd13dbe16e5752352d42
Maximilian Prusch <maximilian.prusch@outlook.com>
2020-05-02 22:32:18 +0200

    Refactor and seperate hooks

---
+++ src/hooks/useRating/index.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { API } from 'aws-amplify';
+import useUser from '../useUser';
+import useRating from './index';
+
+jest.mock('../useUser');
+
+const UseRatingHookExample = () => {
+  const { data: ratings } = useRating();
+  return (
+    <>
+      <p>
+        Ratings:
+        {JSON.stringify(ratings)}
+      </p>
+    </>
+  );
+};
+
+describe('useRating', () => {
+  it('should return mocked ratings', async () => {
+    useUser.mockReturnValue({ userId: { data: 'userId' }, token: { data: 'token' } });
+    render(<UseRatingHookExample />);
+    expect(await screen.findByText(`Ratings:${JSON.stringify([{
+      band: 'Bloodbath',
+      festival: 'Wacken',
+      year: 2015,
+      rating: 5,
+      comment: 'comment',
+    }])}`));
+    expect(API.get).toHaveBeenCalledWith('musicrating',
+      '/api/v1/ratings/bands/userId',
+      { header: { Authorization: 'Bearer token' } });
+    expect(API.get).toHaveBeenCalledTimes(1);
+  });
+  it('should return no ratings when userId is undefined', () => {
+    useUser.mockReturnValue({ userId: { data: undefined }, token: { data: 'token' } });
+    render(<UseRatingHookExample />);
+    expect(screen.getByText('Ratings:'));
+  });
+  it('should return no ratings when token is undefined', () => {
+    useUser.mockReturnValue({ userId: { data: 'userId' }, token: { data: undefined } });
+    render(<UseRatingHookExample />);
+    expect(screen.getByText('Ratings:'));
+  });
+});
--- src/hooks/useUser/index.test.jsx
+++ src/hooks/useUser/index.test.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { API, Auth } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import useUser from './index';
 
 const UseUserHookExample = () => {
-  const { userId, token, ratedArtists } = useUser();
+  const { userId, token } = useUser();
   return (
     <>
-      <p>{userId}</p>
-      <p>{token}</p>
-      <p>{JSON.stringify(ratedArtists)}</p>
+      <p>{userId.data}</p>
+      <p>{token.data}</p>
     </>
   );
 };
 
 describe('useUser', () => {
-  it('should get userId, token and ratedArtists', async () => {
+  it('should get userId and token', async () => {
     const getJwtTokenMock = jest.fn().mockReturnValueOnce('token');
     Auth.currentSession.mockReturnValueOnce({
       getAccessToken: () => ({ getJwtToken: getJwtTokenMock }),
@@ -24,35 +23,7 @@ describe('useUser', () => {
 
     expect(await screen.findByText('userId')).toBeInTheDocument();
     expect(await screen.findByText('token')).toBeInTheDocument();
-    expect(await screen.findByText(JSON.stringify([{
-      band: 'Bloodbath', festival: 'Wacken', year: 2015, rating: 5, comment: 'comment',
-    }]))).toBeInTheDocument();
     expect(Auth.currentUserInfo).toHaveBeenCalledTimes(1);
     expect(getJwtTokenMock).toHaveBeenCalledTimes(1);
-    expect(API.get).toHaveBeenCalledWith('musicrating', '/api/v1/ratings/bands/userId', { header: { Authorization: 'Bearer token' } });
-    expect(API.get).toHaveBeenCalledTimes(1);
-  });
-
-  it('should show default value for ratedArtists when userId was not fetched', async () => {
-    Auth.currentUserInfo.mockResolvedValueOnce({ id: '' });
-    render(<UseUserHookExample />);
-
-    expect(await screen.findByText('token')).toBeInTheDocument();
-    expect(screen.queryByText('userId')).toBeNull();
-    expect(await screen.findByText('[]')).toBeInTheDocument();
-    expect(API.get).not.toHaveBeenCalled();
-  });
-
-  it('should show default value for ratedArtists when token was not fetched', async () => {
-    const getJwtTokenMock = jest.fn().mockReturnValueOnce('');
-    Auth.currentSession.mockReturnValueOnce({
-      getAccessToken: () => ({ getJwtToken: getJwtTokenMock }),
-    });
-    render(<UseUserHookExample />);
-
-    expect(await screen.findByText('userId')).toBeInTheDocument();
-    expect(screen.queryByText('token')).toBeNull();
-    expect(await screen.findByText('[]')).toBeInTheDocument();
-    expect(API.get).not.toHaveBeenCalled();
   });
 });
