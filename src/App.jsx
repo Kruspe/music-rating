090ff42b8d1ba5dae7bd79e6e68968d81a30cd20
Maximilian Prusch <maximilian.prusch@outlook.com>
2021-07-04 22:04:25 +0200

    Add Authenticator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import Amplify from 'aws-amplify';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 
 import awsExports from './aws-exports';
 import './App.css';
@@ -8,10 +11,28 @@ Amplify.configure(awsExports);
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <div>Hello World</div>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [authState, setAuthState] = useState(AuthState.SignIn);
+  useEffect(() => onAuthUIStateChange((nextAuthState) => {
+    setAuthState(nextAuthState);
+  }), []);
+
+  return authState === AuthState.SignedIn ? (
+    <QueryClientProvider client={queryClient}>
+      <div>Hello World</div>
+    </QueryClientProvider>
+  ) : (
+    <AmplifyAuthenticator initalAuthState={AuthState.SignedIn}>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: 'username' },
+          { type: 'password' },
+          { type: 'email' },
+        ]}
+      />
+    </AmplifyAuthenticator>
+  );
+};
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { QueryClientProvider } from 'react-query';
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
+import { onAuthUIStateChange } from '@aws-amplify/ui-components';
 import App from './App';
 
 jest.mock('react-query');
+jest.mock('@aws-amplify/ui-react');
+jest.mock('@aws-amplify/ui-components');
 
-it('should wrap everything with QueryClientProvider', () => {
+beforeEach(() => {
   QueryClientProvider.mockImplementation(({ children }) => (
     <>
       QueryClientProvider
@@ -12,7 +16,36 @@ it('should wrap everything with QueryClientProvider', () => {
     </>
   ));
 
+  AmplifyAuthenticator.render.mockImplementation(({ children }) => (
+    <>
+      AmplifyAuthenticator
+      <div>
+        {children}
+      </div>
+    </>
+  ));
+  AmplifySignUp.render.mockImplementation(({ formFields }) => {
+    const enabledFormFields = formFields.map((formField) => formField.type).join(',');
+    return (
+      <p>
+        AmplifySignUp:
+        {enabledFormFields}
+      </p>
+    );
+  });
+});
+
+it('should only see login and customized sign up when unauthenticated', () => {
+  render(<App />);
+  expect(screen.getByText('AmplifyAuthenticator')).toBeVisible();
+  expect(screen.getByText(/AmplifySignUp:username,password,email/)).toBeVisible();
+  expect(screen.queryByText(/hello world/i)).toBeNull();
+});
+
+it('should use QueryClientProvider and show app when authenticated', () => {
+  onAuthUIStateChange.mockImplementation((authStateHandler) => authStateHandler('signedin'));
   render(<App />);
   expect(screen.getByText('QueryClientProvider')).toBeVisible();
   expect(screen.getByText(/hello world/i)).toBeVisible();
+  expect(screen.queryByText('AmplifyAuthenticator')).toBeNull();
 });
