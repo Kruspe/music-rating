5f1dc825337d028a20525ca8aeb4f6ecc5103077
Maximilian Prusch <maximilian.prusch@outlook.com>
2022-03-09 14:58:18 +0100

    Upgrade dependencies and remove fetch mock

--- frontend/src/test/mocks.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-console: 0 */
-
-const fetchMock = (path, ...args) => {
-  console.warn('window.fetch is not mocked for this call', path, ...args);
-  return new Error('This must be mocked!');
-};
-
-export default fetchMock;
