309bf510092a20f993d4fab7086dcdf642e37d1c
Maximilian Prusch <maximilian.prusch@outlook.com>
2023-01-20 17:16:24 +0100

    Move RatingCard

--- frontend/src/wacken/index.jsx
+++ frontend/src/wacken/index.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { useAuth0 } from '@auth0/auth0-react';
-import RatingCard from '../rating/RatingCard';
+import RatingCard from '../components/RatingCard';
 
 export default function Wacken() {
   const { getAccessTokenSilently, user } = useAuth0();
