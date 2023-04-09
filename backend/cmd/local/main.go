db029391d2c06de7434c5c7c0007ac2c598eae4a
Maximilian Prusch <maximilian.prusch@outlook.com>
2023-04-09 17:45:30 +0200

    Fix local startup

--- backend/cmd/local/main.go
+++ backend/cmd/local/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kruspe/music-rating/internal/adapter/persistence"
 	. "github.com/kruspe/music-rating/internal/adapter/persistence/persistence_test_helper"
 	"github.com/kruspe/music-rating/internal/api"
+	. "github.com/kruspe/music-rating/internal/api/api_test_helper"
 	"github.com/kruspe/music-rating/internal/model"
 	. "github.com/kruspe/music-rating/internal/model/model_test_helper"
 	"github.com/kruspe/music-rating/internal/usecase"
@@ -24,13 +25,20 @@ func main() {
 	}))
 	useCases := usecase.NewUseCases(repos, festivalStorage)
 
-	http.NewServeMux().Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		r.Header.Set("Authorization", TestToken)
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(200)
 		} else {
 			api.NewApi(useCases, repos, api.NewErrorHandler(log)).ServeHTTP(w, r)
 		}
 	}))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
 }
