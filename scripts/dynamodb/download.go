21318ebe460026ec0aeab8d709dc6019aaba4d21
Maximilian Prusch <maximilian.prusch@outlook.com>
2026-02-20 13:41:58 +0000

    Update DynamoDB local version

--- scripts/dynamodb/download.go
+++ scripts/dynamodb/download.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bytes"
 	"context"
+
 	"github.com/codeclysm/extract/v4"
 	"github.com/kruspe/music-rating/scripts/setup"
 )
 
 func main() {
-	b, err := setup.DownloadFile("https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz")
+	b, err := setup.DownloadFile("https://d1ni2b6xgvw0s0.cloudfront.net/v2.x/dynamodb_local_latest.tar.gz")
 	setup.FailOnError("download dynamodb-local", err)
 
 	err = extract.Gz(context.TODO(), bytes.NewReader(b),
